ae5e67abddcefb33a613986ce449d666e0530994
Mieszko Borodziuk <mieszko.borodziuk@gmail.com>
2020-10-06 18:15:48 +0200

    Update AccountList

--- src/Layouts/Header.js
+++ src/Layouts/Header.js
@@ -1,38 +1,60 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import '../Styles/sass/header.sass';
 import logo from '../Images/EventBook_logo.png';
 import { NavLink } from 'react-router-dom';
+import { StoreContext } from '../store/StoreProvider';
+import LoginForm from '../components/LoginForm/LoginForm';
+import Modal from '../components/Modal/Modal';
 
 
 
 const Header = () => {
-    const [accountList, setAccountList] = useState(false);
+    const [isModalOpenLoginForm, setisModalOpenLoginForm] = useState(false);
+    const [isModalOpenAccountList, setisModalOpenAccountList] = useState(false);
+    const { user, setUser } = useContext(StoreContext);
 
-    const ShowAccountList = () => {
-        setAccountList(!accountList);
-     }
-  
-        return (
-            <>
-                <div className="header">
-                    <div className="logo">
-                        <NavLink to="/"><img src={logo} alt="logo" /></NavLink>
-                    </div>
-                    <div className="navigation-container">
-                        <NavLink to="/addevent"><button className="add_event_btn">Dodaj Event</button></NavLink>
-                        <button onClick={ShowAccountList} className="account_btn">Moje konto</button>
-                    </div>
-                    {accountList ?
-                        <div className="header_account-list" >
-                            <div className="header_account-list_loginBtnContainer"><button className="header_account-list_loginBtn">Log In</button></div>
-                            <div className="header_account-list_newAccountBtn">Create new account</div>
-                        </div> : null
-                    }
-                </div>
 
-            </>
-        );
+    const handleOnClose = () => {
+        setisModalOpenAccountList(false);
+        setisModalOpenLoginForm(false);
+    }
     
+
+
+    const handleOnClick = () => {
+        if (Boolean(user)) {
+            setUser(null)
+        } else {
+            setisModalOpenLoginForm(true);
+        }
+    }
+
+    const setProperlyLabel = Boolean(user) ? 'Log out' : 'Log in'
+
+    const handlerShowAccountList = () => {
+        setisModalOpenAccountList(true);
+    }
+
+    return (
+        <>
+            <div className="header">
+                <div className="logo">
+                    <NavLink to="/"><img src={logo} alt="logo" /></NavLink>
+                </div>
+                <div className="navigation-container">
+                    <NavLink to="/addevent"><button className="add_event_btn">Dodaj Event</button></NavLink>
+                    <button onClick={handlerShowAccountList} className="account_btn">Moje konto</button>
+                </div>
+                <Modal handleOnClose={handleOnClose} isOpen={isModalOpenAccountList} sholudBeCloseOnOutsideClick={true} className="account-list-modal">
+                    <button onClick={handleOnClick} className="header_account-list_loginBtn">{setProperlyLabel}</button>
+                    <div className="header_account-list_newAccountBtn">Create new account</div>
+                </Modal>
+                <LoginForm handleOnClose={handleOnClose} isModalOpen={isModalOpenLoginForm} />
+            </div>
+
+        </>
+    );
+
 }
 
 export default Header;
